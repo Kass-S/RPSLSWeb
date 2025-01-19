c47c4de926255f6e4941ab934e60abc5faae8788
Kass-S <kassiespacek101@gmail.com>
2025-01-19 01:19:23 -0800

    updated  logic

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,6 +1,19 @@
 
 
-let test = document.getElementById('test');
+let CPUsubText1 = document.getElementById('CPUsubText1');
+let CPUsubText2 = document.getElementById('CPUsubText2');
+let CPUsubText3 = document.getElementById('CPUsubText3');
+
+let CPUrockBtn = document.getElementById('CPUrockBtn');
+let CPUpaperBtn = document.getElementById('CPUpaperBtn');
+let CPUscissorBtn = document.getElementById('CPUscissorBtn');
+let CPUlizardBtn = document.getElementById('CPUlizardBtn');
+let CPUspockBtn = document.getElementById('CPUspockBtn');
+
+let CPUSuddenDeathBool = false;
+let CPUBestOf3Bool = false;
+let CPUBestOf5Bool = false;
+let CPUBestOf7Bool = false;
 
 async function CallCPURandom(){
     const promise = await fetch('https://kspacekrpsls-fkfehdbve8a0f9ag.westus-01.azurewebsites.net/RockPaperScissorsLizardSpock/PlayRPSLS');
@@ -9,17 +22,57 @@ async function CallCPURandom(){
     return data;
 }
 
-test.addEventListener('click', async function(){
+
+
+CPUrockBtn.addEventListener('click', async function(){
+    
     let testData = await CallCPURandom(); 
     console.log(testData);
-    let stuff = await CPUSuddenDeath("rock", testData);
-    let stuffText = stuff.toString();
+    let userChoice = 'rock'
+    let stuff = await CPUSuddenDeath(userChoice, testData);
+    let stuffText = await stuff.toString();
     console.log(stuffText);
 })
+CPUpaperBtn.addEventListener('click', async function(){
+
+    // let testData = await CallCPURandom(); 
+    // console.log(testData);
+    // let userChoice = 'paper'
+    // let stuff = await CPUSuddenDeath(userChoice, testData);
+    // let stuffText = await stuff.toString();
+    // console.log(stuffText);
+})
+CPUscissorBtn.addEventListener('click', async function(){
+
+    // let testData = await CallCPURandom(); 
+    // console.log(testData);
+    // let userChoice = 'scissors'
+    // let stuff = await CPUSuddenDeath(userChoice, testData);
+    // let stuffText = await stuff.toString();
+    // console.log(stuffText);
+})
+CPUlizardBtn.addEventListener('click', async function(){
+
+    // let testData = await CallCPURandom(); 
+    // console.log(testData);
+    // let userChoice = 'lizard'
+    // let stuff = await CPUSuddenDeath(userChoice, testData);
+    // let stuffText = await stuff.toString();
+    // console.log(stuffText);
+})
+CPUspockBtn.addEventListener('click', async function(){
+
+    // let testData = await CallCPURandom(); 
+    // console.log(testData);
+    // let userChoice = 'spock'
+    // let stuff = await CPUSuddenDeath(userChoice, testData);
+    // let stuffText = await stuff.toString();
+    // console.log(stuffText);
+})
+
 
 async function CPUSuddenDeath(userChoice, randomCPU){
     let CPUChoice = randomCPU;
-    let outcome = '';
 
     switch(userChoice.toLocaleLowerCase())
     {
@@ -27,24 +80,34 @@ async function CPUSuddenDeath(userChoice, randomCPU){
             switch(CPUChoice)
             {
                 case "rock":
-                    outcome = `CPU guessed - ${CPUChoice}\nYou guessed - ${userChoice}\nYou Tie.`; 
-                    return outcome;    
+                    CPUsubText1.innerText = "You Tie.";
+                    CPUsubText2.innerText = `CPU guessed- ${CPUChoice}`;
+                    CPUsubText3.innerText = `You guessed- ${userChoice}`;
+                    return `CPU guessed - ${CPUChoice}\nYou guessed - ${userChoice}\nYou Tie.`;    
 
                 case "paper":
-                    outcome = `CPU guessed - ${CPUChoice}\nYou guessed - ${userChoice}\nYou Lose.`;
-                    return outcome;
+                    CPUsubText1.innerText = "You Lose.";
+                    CPUsubText2.innerText = `CPU guessed- ${CPUChoice}`;
+                    CPUsubText3.innerText = `You guessed- ${userChoice}`;
+                    return `CPU guessed - ${CPUChoice}\nYou guessed - ${userChoice}\nYou Lose.`;
 
                 case "scissors": 
-                    outcome = `CPU guessed - ${CPUChoice}\nYou guessed - ${userChoice}\nYou Win!`;
-                    return outcome;
+                    CPUsubText1.innerText = "You Win!";
+                    CPUsubText2.innerText = `CPU guessed- ${CPUChoice}`;
+                    CPUsubText3.innerText = `You guessed- ${userChoice}`;
+                    return `CPU guessed - ${CPUChoice}\nYou guessed - ${userChoice}\nYou Win!`;
 
                 case "lizard":
-                    outcome = `CPU guessed - ${CPUChoice}\nYou guessed - ${userChoice}\nYou Win!`;
-                    return outcome;
+                    CPUsubText1.innerText = "You Win!";
+                    CPUsubText2.innerText = `CPU guessed- ${CPUChoice}`;
+                    CPUsubText3.innerText = `You guessed- ${userChoice}`;
+                    return `CPU guessed - ${CPUChoice}\nYou guessed - ${userChoice}\nYou Win!`;
 
                 case "spock":
-                    outcome = `CPU guessed - ${CPUChoice}\nYou guessed - ${userChoice}\nYou Lose.`;
-                    return outcome;
+                    CPUsubText1.innerText = "You Lose.";
+                    CPUsubText2.innerText = `CPU guessed- ${CPUChoice}`;
+                    CPUsubText3.innerText = `You guessed- ${userChoice}`;
+                    return `CPU guessed - ${CPUChoice}\nYou guessed - ${userChoice}\nYou Lose.`;
 
                 default:
                     return "";
@@ -54,18 +117,33 @@ async function CPUSuddenDeath(userChoice, randomCPU){
             switch(CPUChoice)
             {
                 case "rock":
+                    CPUsubText1.innerText = "You Win!";
+                    CPUsubText2.innerText = `CPU guessed- ${CPUChoice}`;
+                    CPUsubText3.innerText = `You guessed- ${userChoice}`;
                     return `CPU guessed - ${CPUChoice}\nYou guessed - ${userChoice}\nYou Win!`;
                        
                 case "paper":
+                    CPUsubText1.innerText = "You Tie.";
+                    CPUsubText2.innerText = `CPU guessed- ${CPUChoice}`;
+                    CPUsubText3.innerText = `You guessed- ${userChoice}`;
                     return `CPU guessed - ${CPUChoice}\nYou guessed - ${userChoice}\nYou Tie.`;
 
-                case "scissors": 
+                case "scissors":
+                    CPUsubText1.innerText = "You Lose.";
+                    CPUsubText2.innerText = `CPU guessed- ${CPUChoice}`;
+                    CPUsubText3.innerText = `You guessed- ${userChoice}`; 
                     return `CPU guessed - ${CPUChoice}\nYou guessed - ${userChoice}\nYou Lose.`;
 
                 case "lizard":
+                    CPUsubText1.innerText = "You Lose.";
+                    CPUsubText2.innerText = `CPU guessed- ${CPUChoice}`;
+                    CPUsubText3.innerText = `You guessed- ${userChoice}`;
                     return `CPU guessed - ${CPUChoice}\nYou guessed - ${userChoice}\nYou Lose.`;
 
                 case "spock":
+                    CPUsubText1.innerText = "You Win!";
+                    CPUsubText2.innerText = `CPU guessed- ${CPUChoice}`;
+                    CPUsubText3.innerText = `You guessed- ${userChoice}`;
                     return `CPU guessed - ${CPUChoice}\nYou guessed - ${userChoice}\nYou Win!`;
                 default:
                     return "";
@@ -75,18 +153,33 @@ async function CPUSuddenDeath(userChoice, randomCPU){
             switch(CPUChoice)
             {
                 case "rock":
+                    CPUsubText1.innerText = "You Lose.";
+                    CPUsubText2.innerText = `CPU guessed- ${CPUChoice}`;
+                    CPUsubText3.innerText = `You guessed- ${userChoice}`;
                     return `CPU guessed - ${CPUChoice}\nYou guessed - ${userChoice}\nYou Lose.`;
                         
                 case "paper":
+                    CPUsubText1.innerText = "You Win!";
+                    CPUsubText2.innerText = `CPU guessed- ${CPUChoice}`;
+                    CPUsubText3.innerText = `You guessed- ${userChoice}`;
                     return `CPU guessed - ${CPUChoice}\nYou guessed - ${userChoice}\nYou Win!`;
 
                 case "scissors": 
+                    CPUsubText1.innerText = "You Tie.";
+                    CPUsubText2.innerText = `CPU guessed- ${CPUChoice}`;
+                    CPUsubText3.innerText = `You guessed- ${userChoice}`;
                     return `CPU guessed - ${CPUChoice}\nYou guessed - ${userChoice}\nYou Tie.`;
 
                 case "lizard":
+                    CPUsubText1.innerText = "You Win!";
+                    CPUsubText2.innerText = `CPU guessed- ${CPUChoice}`;
+                    CPUsubText3.innerText = `You guessed- ${userChoice}`;
                     return `CPU guessed - ${CPUChoice}\nYou guessed - ${userChoice}\nYou Win!`;
 
                 case "spock":
+                    CPUsubText1.innerText = "You Lose.";
+                    CPUsubText2.innerText = `CPU guessed- ${CPUChoice}`;
+                    CPUsubText3.innerText = `You guessed- ${userChoice}`;
                     return `CPU guessed - ${CPUChoice}\nYou guessed - ${userChoice}\nYou Lose.`;
 
                 default:
@@ -97,18 +190,33 @@ async function CPUSuddenDeath(userChoice, randomCPU){
             switch(CPUChoice)
             {
                 case "rock":
+                    CPUsubText1.innerText = "You Lose.";
+                    CPUsubText2.innerText = `CPU guessed- ${CPUChoice}`;
+                    CPUsubText3.innerText = `You guessed- ${userChoice}`;
                     return `CPU guessed - ${CPUChoice}\nYou guessed - ${userChoice}\nYou Lose.`;
                         
                 case "paper":
+                    CPUsubText1.innerText = "You Win!";
+                    CPUsubText2.innerText = `CPU guessed- ${CPUChoice}`;
+                    CPUsubText3.innerText = `You guessed- ${userChoice}`;
                     return `CPU guessed - ${CPUChoice}\nYou guessed - ${userChoice}\nYou Win!`;
 
-                case "scissors": 
+                case "scissors":
+                    CPUsubText1.innerText = "You Lose.";
+                    CPUsubText2.innerText = `CPU guessed- ${CPUChoice}`;
+                    CPUsubText3.innerText = `You guessed- ${userChoice}`; 
                     return `CPU guessed - ${CPUChoice}\nYou guessed - ${userChoice}\nYou Lose.`;
 
                 case "lizard":
+                    CPUsubText1.innerText = "You Tie.";
+                    CPUsubText2.innerText = `CPU guessed- ${CPUChoice}`;
+                    CPUsubText3.innerText = `You guessed- ${userChoice}`;
                     return `CPU guessed - ${CPUChoice}\nYou guessed - ${userChoice}\nYou Tie.`;
 
                 case "spock":
+                    CPUsubText1.innerText = "You Win!";
+                    CPUsubText2.innerText = `CPU guessed- ${CPUChoice}`;
+                    CPUsubText3.innerText = `You guessed- ${userChoice}`;
                     return `CPU guessed - ${CPUChoice}\nYou guessed - ${userChoice}\nYou Win!`;
 
                 default:
@@ -119,18 +227,33 @@ async function CPUSuddenDeath(userChoice, randomCPU){
             switch(CPUChoice)
             {
                 case "rock":
+                    CPUsubText1.innerText = "You Win!";
+                    CPUsubText2.innerText = `CPU guessed- ${CPUChoice}`;
+                    CPUsubText3.innerText = `You guessed- ${userChoice}`;
                     return `CPU guessed - ${CPUChoice}\nYou guessed - ${userChoice}\nYou Win.`;
                         
                 case "paper":
+                    CPUsubText1.innerText = "You Lose.";
+                    CPUsubText2.innerText = `CPU guessed- ${CPUChoice}`;
+                    CPUsubText3.innerText = `You guessed- ${userChoice}`;
                     return `CPU guessed - ${CPUChoice}\nYou guessed - ${userChoice}\nYou Lose.`;
 
-                case "scissors": 
+                case "scissors":
+                    CPUsubText1.innerText = "You Win!";
+                    CPUsubText2.innerText = `CPU guessed- ${CPUChoice}`;
+                    CPUsubText3.innerText = `You guessed- ${userChoice}`; 
                     return `CPU guessed - ${CPUChoice}\nYou guessed - ${userChoice}\nYou Win!`
 
                 case "lizard":
+                    CPUsubText1.innerText = "You Lose.";
+                    CPUsubText2.innerText = `CPU guessed- ${CPUChoice}`;
+                    CPUsubText3.innerText = `You guessed- ${userChoice}`;
                     return `CPU guessed - ${CPUChoice}\nYou guessed - ${userChoice}\nYou Lose`;
 
                 case "spock":
+                    CPUsubText1.innerText = "You Tie.";
+                    CPUsubText2.innerText = `CPU guessed- ${CPUChoice}`;
+                    CPUsubText3.innerText = `You guessed- ${userChoice}`;
                     return `CPU guessed - ${CPUChoice}\nYou guessed - ${userChoice}\nYou Tie.`;
 
                 default:
@@ -143,3 +266,141 @@ async function CPUSuddenDeath(userChoice, randomCPU){
     }
 
 }
+
+function CPUBestOf3(userChoice, randomCPU){
+    CPUWins = 0;
+    PlayerWins = 0;
+
+    if(PlayerWins == 2 || CPUWins == 2){
+        if(PlayerWins == 2){
+            
+        }
+    }
+}
+
+
+// test.addEventListener('click', async function(){
+//     let stuff = await PvPSuddenDeath("rock", "Paper");
+//     let stuffText = await stuff.toString();
+//     console.log(stuffText);
+// })
+
+// async function PvPSuddenDeath(P1Choice, P2Choice){
+
+//  p1Turn = true;
+//     switch(P1Choice.toLocaleLowerCase())
+//     {
+//         case "rock": 
+//             switch(P2Choice.toLocaleLowerCase())
+//             {
+//                 case "rock":
+//                     return `Player 1 guessed - ${P1Choice}\nPlayer 2 guessed - ${P2Choice}\nYou Tie.`;    
+
+//                 case "paper":
+//                     return `Player 1 guessed - ${P1Choice}\nPlayer 2 guessed - ${P2Choice}\nPlayer 2 Wins!`;
+//                 case "scissors": 
+//                     return `Player 1 guessed - ${P1Choice}\nPlayer 2 guessed - ${P2Choice}\nPlayer 1 Wins!`;
+
+//                 case "lizard":
+//                     return `Player 1 guessed - ${P1Choice}\nPlayer 2 guessed - ${P2Choice}\nPlayer 2 Wins`;
+
+//                 case "spock":
+//                     return `Player 1 guessed - ${P1Choice}\nPlayer 2 guessed - ${P2Choice}\nPlayer 2 Wins`;
+
+//                 default:
+//                     return "";
+//             }                      
+
+//         case "paper":
+//             switch(P2Choice.toLocaleLowerCase())
+//             {
+//                 case "rock":
+//                     return `Player 1 guessed - ${P1Choice}\nPlayer 2 guessed - ${P2Choice}\nPlayer 1 Wins!`;
+                       
+//                 case "paper":
+//                     return `Player 1 guessed - ${P1Choice}\nPlayer 2 guessed - ${P2Choice}\nYou Tie.`;
+
+//                 case "scissors": 
+//                     return `Player 1 guessed - ${P1Choice}\nPlayer 2 guessed - ${P2Choice}\nPlayer 2 Wins!`;
+
+//                 case "lizard":
+//                     return `Player 1 guessed - ${P1Choice}\nPlayer 2 guessed - ${P2Choice}\nPlayer 2 Wins!`;
+
+//                 case "spock":
+//                     return `Player 1 guessed - ${P1Choice}\nPlayer 2 guessed - ${P2Choice}\nPlayer 1 Wins!`;
+//                 default:
+//                     return "";
+//             }
+
+//         case "scissors": 
+//             switch(P2Choice.toLocaleLowerCase())
+//             {
+//                 case "rock":
+//                     return `Player 1 guessed - ${P1Choice}\nPlayer 2 guessed - ${P2Choice}\nPlayer 1 Wins!`;
+                        
+//                 case "paper":
+//                     return `Player 1 guessed - ${P1Choice}\nPlayer 2 guessed - ${P2Choice}\nPlayer 2 Wins!`;
+
+//                 case "scissors": 
+//                     return `Player 1 guessed - ${P1Choice}\nPlayer 2 guessed - ${P2Choice}\nYou Tie.`;
+
+//                 case "lizard":
+//                     return `Player 1 guessed - ${P1Choice}\nPlayer 2 guessed - ${P2Choice}\nPlayer 1 Wins!`;
+
+//                 case "spock":
+//                     return `Player 1 guessed - ${P1Choice}\nPlayer 2 guessed - ${P2Choice}\nPlayer 2 Wins!`;
+
+//                 default:
+//                     return "";
+//             }
+
+//         case "lizard":
+//             switch(P2Choice.toLocaleLowerCase())
+//             {
+//                 case "rock":
+//                     return `Player 1 guessed - ${P1Choice}\nPlayer 2 guessed - ${P2Choice}\nPlayer 2 Wins!`;
+                        
+//                 case "paper":
+//                     return `Player 1 guessed - ${P1Choice}\nPlayer 2 guessed - ${P2Choice}\nPlayer 1 Wins!`;
+
+//                 case "scissors": 
+//                     return `Player 1 guessed - ${P1Choice}\nPlayer 2 guessed - ${P2Choice}\nPlayer 2 Wins`;
+
+//                 case "lizard":
+//                     return `Player 1 guessed - ${P1Choice}\nPlayer 2 guessed - ${P2Choice}\nYou Tie.`;
+
+//                 case "spock":
+//                     return `Player 1 guessed - ${P1Choice}\nPlayer 2 guessed - ${P2Choice}\nPlayer 1 Wins!`;
+
+//                 default:
+//                     return "";
+//             }
+
+//         case "spock":
+//             switch(P2Choice.toLocaleLowerCase())
+//             {
+//                 case "rock":
+//                     return `Player 1 guessed - ${P1Choice}\nPlayer 2 guessed - ${P2Choice}\nPlayer 1 Wins`;
+                        
+//                 case "paper":
+//                     return `Player 1 guessed - ${P1Choice}\nPlayer 2 guessed - ${P2Choice}\nPlayer 2 Wins`;
+
+//                 case "scissors": 
+//                     return `Player 1 guessed - ${P1Choice}\nPlayer 2 guessed - ${P2Choice}\nPlayer 1 Wins`
+
+//                 case "lizard":
+//                     return `Player 1 guessed - ${P1Choice}\nPlayer 2 guessed - ${P2Choice}\nPlayer 2 Wins`;
+
+//                 case "spock":
+//                     return `Player 1 guessed - ${P1Choice}\nPlayer 2 guessed - ${P2Choice}\nYou Tie.`;
+
+//                 default:
+//                     return "";
+//             }  
+
+//         default:
+//             return "Please enter Rock, Paper, Scissors, Lizard, or Spock.\nPlease check your spelling.";     
+
+//     }
+
+// }
